eb3b82a72b5169e4d2ba9924a04bb21f23599712
xk <xk1020@xkdeMacBook-Pro.local>
2018-07-01 22:38:27 +0800

    demo项目提交

---
+++ src/main/java/demo.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: xk
+ * @Date: 2018/7/1
+ * @Version 1.0
+ */
+public class demo {
+    public static void main(String[] args){
+        String string = "111";
+        System.out.print(string);
+    }
+}
---
+++ src/main/java/demo02.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: xk
+ * @Date: 2018/7/1
+ * @Version 1.0
+ */
+public class demo02 {
+}
